b5ac710bd3f6d4b1b8fb3c97cc785c7070529cb5
TAKAHASHI Kazunari <takahashi@1syo.net>
2016-01-26 18:10:20 +0900

    wip

--- lib/hooks/codeship/helper.rb
+++ lib/hooks/codeship/helper.rb
@@ -1,6 +1,16 @@
 module Idobata::Hook
   class Codeship
     module Helper
+      def label_class(status)
+        case status
+          when 'success'
+            'label-success'
+          when 'error','stopped','infrastructure_failure'
+            'label-danger'
+          else
+            'label-default'
+          end
+        end
     end
   end
 end
--- spec/codeship_spec.rb
+++ spec/codeship_spec.rb
@@ -1,11 +1,15 @@
 describe Idobata::Hook::Codeship, type: :hook do
-  let(:payload) { fixture_payload("codeship/#{fixture}.json") }
+  let(:payload) { fixture_payload("codeship/payload.json") }
 
   before do
-    post payload, 'Content-Type' => 'application/x-www-form-urlencoded'
+    post payload, 'Content-Type' => 'application/json'
   end
 
   describe '#process_payload' do
     subject { hook.process_payload }
+
+    it do
+      subject
+    end
   end
 end
